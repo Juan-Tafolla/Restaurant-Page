560cf378d49972abd0d1d4301eda50f7cb733e7e
Juan-Tafolla <juantafolla2001@gmail.com>
2022-08-03 17:49:57 -0500

    main lesson completed, website unfinished

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,22 @@
 export {contact}
 
 const contact = () =>{
-    const contact = document.createElement('div');
-    contact.setAttribute('class','contact');
-    return contact;
+    const container = document.createElement('div');
+    const title = document.createElement('div');
+    const paraPhone = document.createElement('div');
+    const paraLocation = document.createElement('div');
+    const map = document.createElement('div');
+    
+    container.setAttribute('class','container');
+    title.setAttribute('class', 'title');
+    paraPhone.setAttribute('class', 'paraPhone');
+    paraLocation.setAttribute('class', 'paraLocation');
+    map.setAttribute('class', 'map');
+
+    container.appendChild(title);
+    container.appendChild(paraPhone);
+    container.appendChild(paraLocation);
+    container.appendChild(map);
+
+    return container;
 };
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,23 @@
 export { menu }
 
 const menu = () => {
+    const container = document.createElement('div');
     const menu = document.createElement('div');
+    const title = document.createElement('div');        
+    const entries = document.createElement('div');        
+    const food = document.createElement('div');        
+    const drinks = document.createElement('div');
+
+    container.setAttribute('class','container');
     menu.setAttribute('class','menu');
-    return menu;
+    title.setAttribute('class','title');
+    entries.setAttribute('class','entries');
+    food.setAttribute('class','food');
+    drinks.setAttribute('class','drinks');
+    menu.appendChild(title);
+    menu.appendChild(entries);
+    menu.appendChild(food);
+    menu.appendChild(drinks);
+    container.appendChild(menu);
+    return container;
  };
