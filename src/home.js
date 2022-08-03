e283cf023c7cb4900a4b3d2609b4c8680f7ab77a
Juan-Tafolla <juantafolla2001@gmail.com>
2022-08-02 22:07:15 -0500

    hero layout

--- src/home.js
+++ src/home.js
@@ -3,12 +3,18 @@ export {
 }
 
  const hero = ()=> {
-       
-    const hero = document.createElement('div');
-    hero.setAttribute('class','hero');
-    hero.textContent = 'The Green Dragon Inn';
 
-    return hero;
+    const container = document.createElement('div');    
+    const hero = document.createElement('div');
+    const paraHero = document.createElement('p');
+    container.setAttribute('class','container')
+    hero.setAttribute('class','hero');
+    paraHero.setAttribute('class','paraHero');
+    hero.textContent = 'The Green Dragon Inn';
+    paraHero.textContent = 'The Green Dragon Inn';
+    container.appendChild(hero);
+    container.appendChild(paraHero);
+    return container;
  };
  const main = ()=>{
     console.log('Hi from home.js'); 
